62edd544d56e3d61b653509226647fc9e65730c8
Alisson <alissonroberto25@gmail.com>
2019-09-28 19:19:23 -0300

    alteraçoes botoes

--- js/main.js
+++ js/main.js
@@ -368,4 +368,10 @@
 
     })();
 
+    $(document).ready(function(){
+        $('h3.accordion').click(function(){
+            $(this).parent().find('div.accordion').slideToggle("slow");
+        });
+    });
+
 })(jQuery);
